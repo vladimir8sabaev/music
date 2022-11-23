63f60c74435bc4245fc5b54d8ed071e78817b852
Vladimir <vladimir8sabaev@mail.ru>
2022-11-23 18:19:35 +0300

    made dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,67 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync");
-const sass = require("gulp-sass")(require("sass"));
-const cleanCSS = require("gulp-clean-css");
-const autoprefixer = require("gulp-autoprefixer");
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-const imagemin = require("gulp-imagemin");
-const htmlmin = require("gulp-htmlmin");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const webp = require('gulp-webp');
 
-gulp.task("server", function () {
-  browserSync({
-    server: {
-      baseDir: "src",
-    },
-  });
+gulp.task('server', function() {
 
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+    browserSync({
+        server: {
+            baseDir: "dist"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task("styles", function () {
-  return gulp
-    .src("src/sass/**/*.+(scss|sass)")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(rename({ suffix: ".min", prefix: "" }))
-    .pipe(autoprefixer())
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task("watch", function () {
-  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
 });
 
-gulp.task("default", gulp.parallel("watch", "server", "styles"));
+gulp.task('html', function(){
+    return gulp.src("src/*.html")
+        // .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+
+});
+
+gulp.task('scripts', function(){
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+
+});
+
+gulp.task('icons', function(){
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+// gulp.task('images', function(){
+//     return gulp.src("src/img/**/*")
+//         .pipe(imagemin())
+//         .pipe(gulp.dest("dist/img"));
+// });
+
+gulp.task('images-webp', function(){
+    return gulp.src("src/img/**/*")
+        .pipe(webp())
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'icons', 'html', 'images-webp'));
